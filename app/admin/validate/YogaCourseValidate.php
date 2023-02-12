32d7a8bb932fe7501190ef73467032e5137a18e5
‘e <it_z517987404@qq.com>
2023-02-13 02:04:09 +0800

    1

--- app/admin/validate/YogaCourseValidate.php
+++ app/admin/validate/YogaCourseValidate.php
@@ -1,20 +1,23 @@
 <?php
 
-
 namespace app\admin\validate;
+
 use think\Validate;
 
 class YogaCourseValidate extends Validate
 {
+
     protected $rule = [
         'cate_id' => 'require',
         'title' => 'require',
         'course_url' => 'require',
+        'course_duration' => 'require',
     ];
-
     protected $message = [
         'cate_id.require' => '所屬分類不能為空',
         'title.require' => '文章標題不能為空',
         'course_url.require' => '課程鏈接不能為空',
+        'course_duration.require' => '課程時長不能為空',
     ];
+
 }
